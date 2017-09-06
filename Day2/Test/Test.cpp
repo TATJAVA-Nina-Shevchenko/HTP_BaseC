3297c2053833e71fb534504cd93b1df4380815b3
Nina Shevchenko <Nina_Shevchenko@epam.com>
2017-09-06 17:34:57 +0300

    add day8 project

--- Day2/Test/Test.cpp
+++ Day2/Test/Test.cpp
@@ -2,42 +2,19 @@
 #include <clocale>
 #include <string.h>
 
-void testStr(char*);
 
-void get_request_string(char* request)
-{
-	request[0] = 0;
-	strcat(request, "+");
+void func(char &str){
+	printf("I'm here %s", str);
 }
 
 void main(void)
 {
-	//char request[100]="";
-
-	//strcat(request, "step1");
-	//get_request_string(request);
-	//printf("What in request is - %s", request);
-
-	char one[10]="test";
-	char two[5]="test";
-
-	if("test" == "test"){
-		puts("They are equal");
-	}
-
-	if(*one == *two){
-		puts("And these two also");
-	}else{
-		puts("And these two are not");
-	}
-
-	if(one == two){
-		puts("And these two also");
-	}else{
-		puts("And these two are not");
-	}
-
+	char *string = "Hello";
+	func((char &)string);
+	puts("");
+			
 }
+	
 
 
 

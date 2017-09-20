22559b3f8b352370d005f52e4151037e12268720
Nina Shevchenko <Nina_Shevchenko@epam.com>
2017-09-20 17:22:17 +0300

    refactoring

--- Practice/Day11/Tasks.cpp
+++ Practice/Day11/Tasks.cpp
@@ -1,26 +1,55 @@
 #include <stdio.h>
 #include <string.h>
 
+void writeToFile(FILE *);
+void reversedRead(FILE *);
 
 void main(void){
 
 	FILE *f ;
-	if((f=fopen("numbers.txt", "w"))== NULL){
+
+// write to file
+
+	//if((f=fopen("numbers.txt", "w"))== NULL){
+	//	printf("Error. File couldn't be open.");
+	//	return;
+	//}
+
+	//puts("Enter numbers or EOF (ctrl + z) to end input.");
+	//int input;
+	//scanf("%d",&input);
+
+	//while(!feof(stdin)) {
+	//	fprintf(f,"%d ", input);
+	//	scanf("%d",&input);
+	//} 
+
+	//fclose(f);
+
+// reversed read
+
+	if ((f = fopen("numbers.txt","r"))==NULL){
 		printf("Error. File couldn't be open.");
 		return;
 	}
 
-	puts("Enter numbers or EOF (ctrl + z) to end input.");
-	int input;
-	scanf("%d",&input);
+	char nextChar;
+	char result[250], *buf;
+	for(int i = -1, k = 0 ; ftell(f) != 0; i -= 2){
+		
+		fseek(f, i*sizeof(char), SEEK_END);
+		nextChar = fgetc(f);
 
-	while(!feof(stdin)) {
-		fprintf(f,"%d ", input);
-		scanf("%d",&input);
+		if(nextChar != ' '){
+			k++;
+		}else{
+			fgets(buf, k, f);
+			strcat(result, buf);
+			k = 0;
+		}
 	} 
 
-	fclose(f);
-
-
+	printf(result);
+	
 
 }
